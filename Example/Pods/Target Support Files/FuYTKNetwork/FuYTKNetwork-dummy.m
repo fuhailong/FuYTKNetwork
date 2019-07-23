0d8228965587ef3e800648111470a5dfb9d91f9d
Fu Hailong <fuhailong@miaomiaoz.com>
2019-07-23 18:03:06 +0800

    test

---
+++ Example/Pods/Target Support Files/FuYTKNetwork/FuYTKNetwork-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_FuYTKNetwork : NSObject
+@end
+@implementation PodsDummy_FuYTKNetwork
+@end
---
+++ Example/Pods/Target Support Files/Pods-FuYTKNetwork_Tests/Pods-FuYTKNetwork_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FuYTKNetwork_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_FuYTKNetwork_Tests
+@end
